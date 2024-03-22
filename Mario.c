9478fc86ca83c91d1eb1327caa6058024bb850d9
ZachJaffe <156367105+ZachJaffe@users.noreply.github.com>
2024-03-22 14:11:21 -0500

    Update Mario.c
    Generates pyramids with gap in the middle similar to those seen in Mario

--- Mario.c
+++ Mario.c
@@ -1,15 +1,19 @@
 //Program to generate pyramids out of #'s similar to what is seen in Mario world
+//Libraries
 #include <cs50.h>
 #include <stdio.h>
 
+//Main Function
 int main(void)
 {
     int num;
     // prompt user for positive integer
+    //Input Size Desired
     do
     {
         num = get_int("Size: ");
     }
+    //Determine desired size and print appropriately
     while (num < 1);
     num += 1;
     int m = num - 1;
